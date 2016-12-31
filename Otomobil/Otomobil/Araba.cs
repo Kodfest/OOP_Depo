30e39352322bdbd4679f30176ead44d4400a0237
yavuzgedik <yavuzgedik@outlook.com>
2016-12-31 16:07:10 +0200

    Otomobil Ver.1

---
+++ Otomobil/Otomobil/Araba.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otomobil
+{
+    public class Araba
+    {
+        public string seri { get; set; }
+        public string model { get; set; }
+        public int motorHacmi { get; set; }
+        public int motorGucu { get; set; }
+
+        public string Yazdir()
+        {
+            return string.Format("Seri \t\t: {0} \nModel \t\t: {1} \nMotor Hacmi \t: {2} \nMotor Gücü \t: {3}", seri, model, motorHacmi, motorGucu);
+        }
+
+        public virtual string Calistir()
+        {
+            return "Araba Çalıştı";
+        }
+    }
+}
---
+++ Otomobil/Otomobil/Porsche.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otomobil
+{
+    public class Porsche : Araba
+    {
+        public Porsche()
+        {
+            seri = "GT";
+            model = "911";
+            motorHacmi = 3000;
+            motorGucu = 300;
+        }
+
+        public override string Calistir()
+        {
+            return "Vrom Vrom Vrom";
+        }
+    }
+}
